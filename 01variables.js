a38bb216e46f6f1c98301b7949dfcdfd45505578
swatantra <swatantraa30@gmail.com>
2021-04-09 15:18:24 +0530

    javascript commited variables file

---
+++ 01variables.js
@@ -0,0 +1,33 @@
+// var fullName = "swatantra";
+// console.log(fullName);
+
+// var creditCard;
+
+// creditCard = "you are hacked";
+// console.log(creditCard);
+
+//signup form for a website sample account
+var userId = "12344321";
+
+var fullName = "swatantra gupta";
+
+var email = "swatantraa30@gmail.com";
+
+var mobileNo = "9084440850";
+
+var password = "1234swa";
+
+var confirmPassword = "1234swa";
+
+var age = "23";
+
+var address = "village post office lohvan mathura";
+
+console.log("user id is : ",userId);
+console.log("full name is : ",fullName);
+console.log("email id is : ",email);
+console.log("mobile number is : ",mobileNo);
+console.log("password is : ",password);
+console.log("confirmed password is : ",confirmPassword);
+console.log("age of user is : ",age);
+console.log("address of user is : ",address);
